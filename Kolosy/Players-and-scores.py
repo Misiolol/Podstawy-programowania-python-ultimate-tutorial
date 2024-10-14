a1710430088c3bc8462882e911c89f24de5e28f6
Igor Komar <komarigor2006@gmail.com>
2024-10-14 21:32:28 +0200

    DZIALA PLAYERS

--- Kolosy/Players-and-scores.py
+++ Kolosy/Players-and-scores.py
@@ -1,22 +1,50 @@
 N = int(input())
-
+scores = []
 players = set()
-input = []
-for _ in range (N):
-    cutternt_player = list(input().split())
-    input.append(cutternt_player)
-    for player in cutternt_player:
-        name, points = player.split(":")
-        players.add(name)
-players_list = []
+for _ in range(N):
+    current_match = list(input().split()) 
+    scores.append(current_match)
+    name = current_match[0].split(":")[0]
+    name2 = current_match[1].split(":")[0]
+    players.add(name)
+    players.add(name2)
+
+players = list(players)
+solution_array = []
+
 for _ in range(len(players)):
-    players_list.append([players[_], 0, 0])
+    solution_array.append([players[_], int(0), int(0)])
 
-for i in range(0, input, 2):
-    name, points = input[i].split(":")
-    name2, points2 = input[i+1].split(":")
-    if points > points2:
-        
+#* print(scores)
+#* print(solution_array)
+for _ in range(0, len(scores)):
+    name, score = scores[_][0].split(":")
+    name2, score2 = scores[_][1].split(":")
+    for i  in range(len(solution_array)):
+        if solution_array[i][0] == name:
+            index = i
+    for i  in range(len(solution_array)):
+        if solution_array[i][0] == name2:
+            index2 = i
+    if int(score) > int(score2):
+        solution_array[index][1]+=1
+        #print(solution_array[index2][0])
+    if int(score2) > int(score):
+        solution_array[index2][1]+=1
+    solution_array[index][2]+=int(score)
+    solution_array[index2][2]+=int(score2)
+
+sorted_solution_array = sorted(solution_array, key=lambda x:(-x[1], -x[2], x[0]))
+#print (sorted_solution_array)
+
+for i in range(len(players)):
+    print(sorted_solution_array[i][0])
 
 
+
+
+
+
+
+#!print(solution_array)
 
