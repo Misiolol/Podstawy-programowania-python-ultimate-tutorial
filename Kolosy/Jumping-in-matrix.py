507d4e3972a96a6a961852ad7f6efa70488ff4c9
Igor Komar <komarigor2006@gmail.com>
2024-10-24 00:56:11 +0200

    Added jumping in matrix and easier matrix printing solution for debugging purposes

---
+++ Kolosy/Jumping-in-matrix.py
@@ -0,0 +1,55 @@
+#import _Functions
+
+def move_to_column(arr,x,y):
+    sub_arr = []
+    for i in range(len(arr)):
+        sub_arr.append(arr[i][y])
+    if arr[x][y] == min(sub_arr):
+        return -1
+    else:
+        move_to = 0
+        for i in range(len(arr)):
+            if arr[i][y] == min(sub_arr):
+                move_to = i
+        return move_to  
+    
+def move_to_row(arr,x,y):
+    sub_arr = []
+    for i in range(len(arr)):
+        sub_arr.append(arr[x][i])
+    if arr[x][y] == min(sub_arr):
+        return -1
+    else:
+        move_to = 0
+        for i in range(len(arr)):
+            if arr[x][i] == min(sub_arr):
+                move_to = i
+        return move_to  
+      
+def solution(arr,x,y):
+    markerx, markery = x,y
+    while True:
+        k = move_to_row(arr,markerx, markery)
+        if k != -1:
+            markery = k
+        k2 = move_to_column(arr,markerx, markery)
+        if k2!=-1:
+            markerx = k2
+        if k == -1 and k2 == -1:
+            return [markerx, markery]
+
+
+def main():    
+    size = int(input())
+    x,y = map(int, input().split())
+    
+    matrix = [list(map(int, input().split())) for _ in range(size)]
+    point = solution(matrix,x,y)
+    print('{} {}'.format(
+        point[0],
+        point[1]
+    ))
+    #? _Functions.matrix_print(matrix)
+    return 0
+
+main()
---
+++ Kolosy/_Functions.py
@@ -0,0 +1,6 @@
+def matrix_print(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print(arr[i][j], end=" ")
+        print("")
+    print(" ")
