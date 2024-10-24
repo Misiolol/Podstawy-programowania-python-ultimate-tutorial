70ea99c2bd221f336920952e801899c35479b1b8
misiolol <komarigor2006@gmail.com>
2024-10-24 19:16:50 +0200

    to repair matrix trans

---
+++ Kolosy/Matrix-trnsformations.py
@@ -0,0 +1,64 @@
+#import _Functions
+def matrix_print(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            print(arr[i][j], end=" ")
+        print("")
+    print(" ")
+
+
+def reverse_row(arr, idx: int):
+    sub = []
+    sub = arr[idx]
+    sub = sub[::-1]
+    arr[idx] = sub
+    #_Functions.matrix_print(arr)
+    return arr
+
+def reverse_column(arr, idx:int):
+    sub = []
+    for i in range(len(arr)):
+        sub.append(arr[i][idx])
+        sub.reverse()
+        arr[i][idx] = sub[i]
+    return arr
+
+def transchuj(arr):
+    subarr = []
+    for _ in range(len(arr[0])):
+        temp = []
+        for k in range(len(arr)):
+            temp.append(0)
+        subarr.append(temp)
+
+    #print(subarr)
+
+    for i in range(len(arr)):
+        for j in range(len(arr[i])):
+            subarr[j][i] = arr[i][j]
+
+    return subarr
+    
+def solution(todo, matrix):
+    mtx = matrix
+    for task in todo:
+        if task[0] == "R":
+            task,idx = task.split(" ")
+        if task == "RR":
+            mtx = reverse_row(mtx, int(idx))
+        if task == "RC":
+            mtx = reverse_column(mtx, int(idx))
+        if task == "T":
+            mtx = transchuj(mtx)
+    return mtx
+
+
+def main():
+    x,y = map(int, input().split())
+    matrix = [list(map(str, input().split())) for _ in range(x)]
+    n = int(input())
+    to_do = []
+    for _ in range(n):
+        to_do.append(input())
+    matrix_print(solution(to_do, matrix))
+main()
--- Kolosy/pomoc.py
+++ Kolosy/pomoc.py
@@ -1,4 +1,1 @@
-arr = [[0 for _ in range(20)] for _ in range(40)]
-
-
-print(arr)  
+n=int(input());students=[];degrees=[];[(students.append([s:=row[0],(suma_ocen:=(sum(float(wartosc) for test, wartosc in (o.split(':') for o in row[1:])))/len(row[1:])]))), [degrees.append([test, [float(wartosc)]]) if test not in [d[0] for d in degrees] else [d[1].append(float(wartosc)) for d in degrees if d[0] == test][0] for test, wartosc in (o.split(':') for o in row[1:])]) for row in [input().split()]];print('\n'.join(f'{s[0]} {s[1]}' for s in sorted(students, key=lambda x: x[0])));print('\n'.join(f'{d[0]} {sum(d[1])/len(d[1])}' for d in sorted(degrees, key=lambda x: x[0])))
