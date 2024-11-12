e4b756834e9497c8deb336d2f2d2ed18073d3664
Igor Komar <komarigor2006@gmail.com>
2024-11-12 22:01:02 +0100

    xddd

--- Kolosy/Pictures.py
+++ Kolosy/Pictures.py
@@ -1,8 +1,26 @@
-x,y,n,m = map(int, input().split())
+def solution(arr, n: int, m: int):
+    x, y = len(arr), len(arr[0])
+    for i in range(x - n + 1):
+        for j in range(y - m + 1):
+            is_zeroes = True
+            for k in range(n):
+                for l in range(m):
+                    if arr[i + k][j + l] != 0:
+                        is_zeroes = False
+                        break
+                if not is_zeroes:
+                    break
+            if is_zeroes:
+                return True
+    return False
 
-wall = [list(map(int, input().split())) for _ in range(x)]
+def main():
+    x,y,n,m = map(int, input().split())
+    arr = [list(map(int, input().split())) for _ in range(x)]
+    if solution(arr, n, m):
+        print("True")
+    else:
+        print("False")
+        
+main()
 
-print(wall[0][0])
-
-def a():
-    return 0
